1f6e24b99fe7b35384494ad60925545bd809a765
rdosreis <rodrigocpdosreis@gmail.com>
2021-08-26 02:07:23 -0300

    aula 00

---
+++ RScripts/00_apresenta_curso.R
@@ -0,0 +1,83 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', out.height='80%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'hi_my_name_is.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
+knitr::include_graphics(here::here('images','videoaula-2.png'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='10%', paged.print=FALSE----
+knitr::include_graphics(here::here('images','Rlogo.png'))
+
+
+## ---- echo=FALSE, eval=TRUE--------------------------------------------------------------------
+
+x <- rnorm(n = 100, mean = 10, sd = 1)
+
+
+
+## ---- echo=TRUE, eval=TRUE, fig.align='center', out.width='50%'--------------------------------
+
+hist(x, col = 'black', border = 'white')
+
+
+
+## ----echo=FALSE, fig.align='right', message=FALSE, warning=FALSE, out.width='15%', paged.print=FALSE----
+knitr::include_graphics(here('images','ctanlion.png'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'arvore-processo-envelhecimento.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'processo-envelhecimento.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'ecr-01.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'ecr-02.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'graunt.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'huygens.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'cox.jpg'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'counting.png'))
+
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', out.height='50%', paged.print=FALSE----
+
+knitr::include_graphics(here::here('images', 'Statistically-Insignificant-8.jpg'))
+
+
